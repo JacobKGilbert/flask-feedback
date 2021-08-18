13bb8dd020fc68de086e3adfece1571da6925217
Jacob Gilbert <jacobkgilbert@gmail.com>
2021-08-18 14:37:20 -0500

    added registration functionality

---
+++ app.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template, redirect, session
+
+from models import connect_db, db, User
+from forms import RegisterForm
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgres:///flask-feedback"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
+app.config['SECRET_KEY'] = "shhhhh"
+
+connect_db(app)
+
+
+@app.route("/")
+def home_rt():
+    """Home route, redirects to register."""
+
+    return redirect("/register")
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """Register a user: produce form and handle form submission."""
+
+    if "username" in session:
+        return redirect(f"/users/{session['username']}")
+
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        first_name = form.first_name.data
+        last_name = form.last_name.data
+        email = form.email.data
+
+        user = User.register(username, password, first_name, last_name, email)
+
+        db.session.commit()
+        session['username'] = user.username
+
+        return redirect(f"/users/{user.username}")
+
+    else:
+        return render_template("users/register.html", form=form)
